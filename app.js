afd6d03fcbbfbe9f5ce54b4cf02ef91a8950465f
ruoyu <hunger@jirengu.com>
2017-08-03 16:03:01 +0800

    chat

---
+++ app.js
@@ -0,0 +1,22 @@
+
+let path = require('path')
+let express = require('express')
+let app = express()
+let server = require('http').createServer(app)
+let io = require('socket.io')(server)
+
+app.use(express.static(path.join(__dirname, '/public')))
+server.listen(3000)
+
+
+
+io.on('connection', function(client){
+  client.on('message', function(data){
+    client.broadcast.emit('message', data)
+    console.log(data)
+  });
+  client.on('disconnect', function(){
+    console.log('disconnect...')
+  });
+});
+
---
+++ public/main.js
@@ -0,0 +1,64 @@
+/*
+var app = {
+  init: function(){
+
+  },
+
+  bind: function(){
+
+  }
+
+}
+
+app.init()
+
+*/
+
+
+var socket = io()
+
+
+if(localStorage.name){
+  document.querySelector('footer .name').innerText = localStorage.name
+}
+
+
+socket.on('message', function(data) {
+  console.log('server send...')
+  console.log(data)
+  addMessage(data.name, data.message)
+})
+
+
+document.querySelector('footer input').onchange = function(){
+  var name = localStorage.name || '游客'
+  var message = this.value
+  addMessage(name, message)
+  this.value = ''
+  socket.emit('message', {
+    name: name,
+    message: message
+  })
+}
+
+document.querySelector('footer .name').onblur = function(e){
+  console.log(this.value)
+  localStorage.name = this.innerText
+}
+
+
+function addMessage(name, msg){
+  var msgNode = document.createElement('div')
+  var childSpan = document.createElement('span')
+  childSpan.innerText = name + '说:'
+  var childText = document.createTextNode(msg)
+  msgNode.appendChild(childSpan)
+  msgNode.appendChild(childText)
+  msgNode.classList.add('message')
+  setTimeout(function(){
+    msgNode.classList.add('normal')
+  }, 2000)
+  document.body.querySelector('main').appendChild(msgNode)
+  console.log(getComputedStyle(msgNode).width)
+}
+
